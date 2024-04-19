b00d9c648297aab384c14dfd3b799e1f6e88ac3d
Ozod <o.bukhorizoda@mail.ru>
2024-04-19 23:09:09 +0500

    add db.json file

--- src/pages/home/ui/index.tsx
+++ src/pages/home/ui/index.tsx
@@ -1,18 +1,16 @@
-import { LoginCurve } from "iconsax-react"
-import UI from "../../../shared/ui"
+import { LoginCurve } from "iconsax-react";
+import UI from "../../../shared/ui";
 
 const Home = () => {
-	return (
-		<div className="w-full">
-			<div className="rounded-small px-7">
+  return (
+    <div className="w-full">
+      <div className="rounded-small px-7"></div>
+      <UI.Button size="md" color="default">
+        {/* <LoginCurve size={17}/> */}
+        login
+      </UI.Button>
+    </div>
+  );
+};
 
-			</div>
-			<UI.Button size="md" color="default">
-				{/* <LoginCurve size={17}/> */}
-				login
-			</UI.Button>
-		</div>
-	)
-}
-
-export default Home
+export default Home;
--- src/pages/shop/ui/shop.content.tsx
+++ src/pages/shop/ui/shop.content.tsx
@@ -1,13 +1,16 @@
 const Shop = () => {
   return (
     <>
-      <div className="flex flex-col justify-center items-start">
-        <img
-          src="https://i.postimg.cc/Wbgy393N/product-21-320x320-1.png"
-          alt="Plant"
-        />
-
-        <p>Barberton Daisy</p>
+      <div className="flex flex-wrap gap-2 justify-start items-start w-full">
+        <div className="flex flex-col">
+          <div className="border-large">
+            <img
+              src="https://i.postimg.cc/Wbgy393N/product-21-320x320-1.png"
+              alt="Plant"
+            />
+          </div>
+          <p>Barberton Daisy</p>
+        </div>
       </div>
     </>
   );
