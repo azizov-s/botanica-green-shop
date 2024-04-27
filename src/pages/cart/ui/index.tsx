9b15ec88fb51fd8c42dcac09f62b6dcc6d2a5aba
Ozod <o.bukhorizoda@mail.ru>
2024-04-27 22:28:31 +0500

    add cart page

--- src/pages/cart/ui/index.tsx
+++ src/pages/cart/ui/index.tsx
@@ -1,4 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
+import UI from "../../../shared/ui";
+import { Divider } from "@nextui-org/react";
+interface Item {
+  id: number | string;
+  img: string;
+  price: string;
+  name: string;
+}
 
 const Cart = () => {
   const {
@@ -16,10 +24,46 @@ const Cart = () => {
   if (isLoading) return <div>Loading...</div>;
   return (
     <>
-      <div>
-        {cart?.map((item) => {
-          return <div key={item?.id}>{item?.name}</div>;
-        })}
+      <div className="flex w-full justify-between gap-20">
+        <div className="flex flex-col gap-3 flex-1">
+          {cart?.map((item: Item) => {
+            return (
+              <>
+                <div className="flex items-center justify-between bg-[#FBFBFB] w-full p-2 gap-5">
+                  <div className="w-[170px]">
+                    <img className="max-w-full" src={item?.img} alt="Product" />
+                  </div>
+                  <div className="w-full">{item?.name}</div>
+                  <div>{item?.price}</div>
+                  <div className="flex justify-start items-center gap-2 mt-3">
+                    <UI.Button
+                      size="sm"
+                      className="bg-[green] rounded-3xl  p-5 text-[#fff]"
+                    >
+                      -
+                    </UI.Button>
+                    <UI.Input
+                      size="md"
+                      color="default"
+                      className="w-[50px]"
+                      placeholder="0"
+                    />
+                    <UI.Button
+                      size="sm"
+                      className="bg-[green] rounded-3xl  p-5 text-[#fff]"
+                    >
+                      +
+                    </UI.Button>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+        <div className="flex-1">
+          <div>Cart Totals</div>
+          <Divider />
+        </div>
       </div>
     </>
   );
